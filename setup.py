911601f50f87d5d573d649d0767b6c11f1818c0d
Nick Ross <nick.ir.ross@gmail.com>
2019-01-23 12:09:13 +0000

    Clean up readme a bit

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points='''
         [ckan.plugins]
-        defrareports=ckanext.defrareports.plugin:DefraeportsPlugin
+        defrareports=ckanext.defrareports.plugin:DefraReportsPlugin
 
         [babel.extractors]
         ckan = ckan.lib.extract:extract_ckan
