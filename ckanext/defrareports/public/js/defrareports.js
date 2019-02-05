d48fdd53d45debb0661ea425667a5974790af555
Nick Ross <nick.ir.ross@gmail.com>
2019-02-05 11:10:50 +0000

    Use ckan's resource manager for static files

--- ckanext/defrareports/public/js/defrareports.js
+++ ckanext/defrareports/public/js/defrareports.js
@@ -1,6 +1,20 @@
-$(document).ready(function(){
-  $('.expando-report').on('click', function(){
-    var chev = $(this).siblings('ul').get(0)
-    $(chev).toggle();
+var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+$(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+
+  if ($('#report-table').length > 0) {
+    $('#report-table').tablesorter({
+      dateFormat: 'uk',
+    });
+  }
+  $('.js-auto-submit').change(function () {
+      $(this).closest('form').submit();
+  });
+
+  // Hide dataset name tooltip after accordion has closed
+  $('.worst-offender-panel').on('hidden.bs.collapse', function() {
+    $(this).prev().find('[data-toggle="tooltip"]').tooltip('hide');
   });
 });
+
